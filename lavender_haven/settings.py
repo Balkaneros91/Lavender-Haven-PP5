675738611a03bda8c1df4f9d7c627201b2924808
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-06-20 22:28:26 +0000

    Setup base and index html

--- lavender_haven/settings.py
+++ lavender_haven/settings.py
@@ -67,7 +67,11 @@ ROOT_URLCONF = 'lavender_haven.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            TEMPLATES_DIR,
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
