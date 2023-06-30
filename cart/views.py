04977778791cb7b48138a9a222bc860c67489804
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-06-30 22:55:00 +0000

    Add update and delete article in cart views and and subtotal calc filter

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.http import HttpResponse
+from django.contrib import messages
+from articles.models import Article
 
 
 def view_cart(request):
@@ -22,3 +25,38 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     # print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """ Adjust the quantity of the selected article to the specified amount """
+
+    article = get_object_or_404(Article, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request, f'Updated {article.name} quantity to {cart[item_id]}')  # noqa
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed {article.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, item_id):
+    """ Remove article from the shopping cart """
+
+    article = get_object_or_404(Article, pk=item_id)
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        messages.success(request, f'Removed {article.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
