c8be6fb3e65604b205863e339e1a808d2778999e
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-06-30 20:01:48 +0000

    Update context prcessor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from articles.models import Article
 
 
 def cart_contents(request):
@@ -7,6 +9,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     article_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        article = get_object_or_404(Article, pk=item_id)
+        total += item_data * article.price
+        article_count += item_data
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'article': article,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
