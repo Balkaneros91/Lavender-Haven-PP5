18ec0c3b97c21b3ee7708dd3d0661f95c817cf58
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-06-30 19:31:59 +0000

    Add funcionality for add to cart

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view to display the shopping cart """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the selected article to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
