3d295c037e1c7eaeedaa095a1ac84fab09c52175
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-06-30 15:43:22 +0000

    Add funcionality for filtering by category

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Article
+from .models import Article, Category
 
 
 def article_list(request):
@@ -9,8 +9,14 @@ def article_list(request):
 
     articles = Article.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            articles = articles.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -24,6 +30,7 @@ def article_list(request):
     context = {
         'articles': articles,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'articles/articles.html', context)
