42c8f970853e21e56940f3c544cb8463cbb5389b
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-07-02 14:39:09 +0000

    Add stripe elements, basic checkout functionality and logic for checkout success

--- checkout/models.py
+++ checkout/models.py
@@ -67,8 +67,8 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.article.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'SKU {self.article.sku} on order {self.order.order_number}'
