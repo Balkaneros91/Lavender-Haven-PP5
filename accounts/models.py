23f41e7ab8bf67ce9c8d86e7b6ceb79a39aa918b
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-07-03 11:00:41 +0000

    Update allauth templates, test accounts, add accounts form, views and template update

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ from django_countries.fields import CountryField
 
 class UserAccount(models.Model):
     """
-    A user profile model for maintaining default
+    A user account model for maintaining default
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -30,6 +30,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=instance)
+        UserAccount.objects.create(user=instance)
     # Existing users: just save the profile
-    instance.userprofile.save()
+    instance.useraccount.save()
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def account(request):
     """ Display the user's account. """
+    account = get_object_or_404(UserAccount, user=request.user)
+
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')   # noqa
+    else:
+        form = UserAccountForm(instance=account)
+    orders = account.orders.all()
 
     template = 'accounts/account.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_account_page': True
+    }
 
     return render(request, template, context)
