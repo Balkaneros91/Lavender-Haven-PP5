8ee2b1e967f5efd383f1040c31913439d0298893
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-07-04 19:39:48 +0000

    Add blog detail view, urls and template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blog
 
 
@@ -8,3 +8,30 @@ class BlogList(generic.ListView):
     queryset = Blog.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/blog.html'
     paginate_by = 6
+
+
+class BlogDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        blog = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "blog/blog_detail.html",
+            {
+                "blog": blog,
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        blog = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "blog/blog_detail.html",
+            {
+                "blog": blog,
+            },
+        )
