2e3f9d80582fcca07b24f9f5253809b66a6f5521
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-07-04 19:47:26 +0000

    Add blog view, urls and adjust templates for likes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Blog
 
 
@@ -15,23 +16,44 @@ class BlogDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Blog.objects.filter(status=1)
         blog = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         return render(
             request,
             "blog/blog_detail.html",
             {
                 "blog": blog,
+                "liked": liked,
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Blog.objects.filter(status=1)
         blog = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         return render(
             request,
             "blog/blog_detail.html",
             {
                 "blog": blog,
+                "liked": liked,
             },
         )
+
+
+class BlogLike(View):
+
+    def post(self, request, slug):
+        blog = get_object_or_404(Blog, slug=slug)
+
+        if blog.likes.filter(id=self.request.user.id).exists():
+            blog.likes.remove(request.user)
+        else:
+            blog.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('blog_detail', args=[slug]))
