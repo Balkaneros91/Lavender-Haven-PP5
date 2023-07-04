9de93374b717d1949f1c1c462d3fc38cd24eefed
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-07-04 19:32:07 +0000

    Add blog views, urls and templates

--- lavender_haven/urls.py
+++ lavender_haven/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('account/', include('accounts.urls')),
     path('about/', include('about.urls')),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
